a84b8b743ef2f440c0fc1df6ca9c79b62bf8c900
yarkor <korovkin.jaroslav@gmail.com>
2026-01-25 21:09:47 +0400

    добавил текстуры в класс Hero

--- code/sprites.py
+++ code/sprites.py
@@ -1,5 +1,8 @@
 import arcade
 
+GHOST_ANIMATION_SPEED = 0.5
+HERO_ANIMATION_SPEED = 0.25
+
 
 class Enemy(arcade.Sprite):
     def __init__(self, x, y):
@@ -36,12 +39,11 @@ class Spider(Enemy):
             self.texture = self.down_image
 
 
-class Spider(Enemy):
+class Ghost(Enemy):
     def __init__(self, x, y, speed):
         super().__init__(x, y)
         self.textures = [arcade.load_texture("../res/ghost_animation/1.png"),
                          arcade.load_texture("../res/ghost_animation/2.png")]
-        self.animation_speed = 0.5
         self.texture = self.textures[0]
         self.frame = 0
         self.animation_timer = 0
@@ -50,8 +52,33 @@ class Spider(Enemy):
     def update(self, delta_time: float, player_x, player_y):
         super().update(delta_time, player_x, player_y, self.speed)
         self.animation_timer += delta_time
-        if self.animation_timer >= self.animation_speed:
+        if self.animation_timer >= GHOST_ANIMATION_SPEED:
             self.frame += 1
             self.frame %= 2
             self.texture = self.textures[self.frame]
             self.animation_timer = 0
+
+
+class Hero(arcade.Sprite):
+    def __init__(self, x, y):
+        super().__init__()
+        self.center_x = x
+        self.center_y = y
+        self.idle_texture = arcade.load_texture("../res/hero_animation/default.png")
+        self.walk_up_textures = []
+        self.walk_down_textures = []
+        self.walk_left_textures = []
+        self.walk_right_textures = []
+        for i in range(4):
+            self.walk_up_textures.append(
+                arcade.load_texture(f"../res/hero_animation/up{i}.png")
+            )
+            self.walk_down_textures.append(
+                arcade.load_texture(f"../res/hero_animation/down{i}.png")
+            )
+            self.walk_left_textures.append(
+                arcade.load_texture(f"../res/hero_animation/left{i}.png")
+            )
+            self.walk_right_textures.append(
+                arcade.load_texture(f"../res/hero_animation/right{i}.png")
+            )
